9c27cd0b52b36da385a31fe228c7e7cc54f1a238
Ruben Renart <rubenrenart@gmail.com>
2025-02-11 19:24:10 +0100

    v1.0.1 - Corrección en los datos de prueba: Se han modificado las fechas para que estén en el formato que toca. Además, se han arreglado otros errores.

--- app/src/main/java/com/example/pdm_todoapp/ToDoViewModel.kt
+++ app/src/main/java/com/example/pdm_todoapp/ToDoViewModel.kt
@@ -24,19 +24,18 @@ class ToDoViewModel : ViewModel() {
     val isLoginEnabled: LiveData<Boolean> = _isLoginEnabled
 
     init {
-        val lista = listOf<ToDo>(
-            ToDo(0, "Cena Familiar", "Hay que ir bien vestido", "20-02-2025", true, 2),
-            ToDo(1, "Terminar la App de ToDos", "", "18-02-2025", false, 3),
-            ToDo(2, "Terminar el Videojuego", "Estaría bien que se te ocurriera una idea sobre qué hacerlo...", "16-02-2025", false, 3),
-            ToDo(3, "Terminar Proyecto de Angular", "Solo queda el documento extra.",  "14-02-2025", false, 1),
-            ToDo(4, "Terminar Ejercicio de Acceso a Datos", "",  "20-02-2025", false, 2),
-            ToDo(5, "Ir al Cine", "",  "01-03-2025", true, 0),
-            ToDo(6, "Examen Inglés", "",  "17-02-2025", false, 0),
-            ToDo(7, "Examen Bootstrap & Angular", "",  "20-02-2025", false, 2),
-            ToDo(8, "Examen Node & Express", "Se permiten apuntes.",  "13-02-2025", false, 2)
-        )
+        addToDo("Cena Familiar", "Hay que ir bien vestido", "04/04/2025", true, 2)
+        addToDo("Terminar la App de ToDos", "", "18/02/2025", false, 3)
+        addToDo("Terminar el Videojuego", "Estaría bien que se te ocurriera una idea sobre qué hacerlo...", "16/02/2025", false, 3)
+        addToDo("Terminar Proyecto de Angular", "Solo queda el documento extra.",  "14/02/2025", false, 1)
+        addToDo("Terminar Ejercicio de Acceso a Datos", "",  "20/02/2025", false, 2)
+        addToDo("Ir al Cine", "",  "01/03/2025", true, 0)
+        addToDo("Examen Inglés", "",  "17/02/2025", false, 0)
+        addToDo("Examen Bootstrap & Angular", "",  "20/02/2025", false, 2)
+        addToDo("Examen Node & Express", "Se permiten apuntes.",  "13/02/2025", false, 2)
 
-        _toDoList.value = lista
+        _title.value = ""
+        _date.value = ""
     }
 
     fun onTitleChange(title: String)
